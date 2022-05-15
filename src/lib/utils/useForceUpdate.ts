5c3a8ea48c23161885872a3604020ffd8258bc75
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-15 21:03:24 +0200

    Implement useForceUpdate hook.

---
+++ src/lib/utils/useForceUpdate.ts
@@ -0,0 +1,12 @@
+/*
+Author: Ing. Luca Gian Scaringella
+GitHub: LucaCode
+Copyright(c) Ing. Luca Gian Scaringella
+ */
+
+import { useCallback, useState } from "react";
+
+export function useForceUpdate() {
+    const [, setValue] = useState(true);
+    return useCallback(() => setValue((v) => !v), []);
+}
