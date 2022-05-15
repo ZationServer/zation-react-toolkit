98d0b94092407bed92d0d00c3cc3c2e39b48ce93
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-05-15 21:07:15 +0200

    Export hooks.

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/*
+Author: Ing. Luca Gian Scaringella
+GitHub: LucaCode
+Copyright(c) Ing. Luca Gian Scaringella
+ */
+
+import {useClient} from "./lib/useClient";
+import {useDatabox} from "./lib/useDatabox";
+import {useDataboxTracking} from "./lib/useDataboxTracking";
+
+export {
+    useClient,
+    useDatabox,
+    useDataboxTracking
+};
